aacf2604a1b57f58c0ce0044e8c7ad5c038e80ec
Fardin Abdullah <fardindino@gmail.com>
2022-01-22 20:16:34 -0500

    added pictures of clothes

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //getting the location input
 function getLocation(){
     const location = document.getElementById("location")
-    console.log(location.value)
 //API
 fetch("https://api.openweathermap.org/data/2.5/forecast?q="+location.value+"&appid=c9dbfe419caaad8dfe99e90d39c81b98")
 .then(response=>response.json())
@@ -15,4 +14,64 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?q="+location.value+"&app
     }
 })
 
+
+}
+
+//Determing what to wear.
+function getClothing () {
+    const location = document.getElementById("location")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+location.value+"&appid=c9dbfe419caaad8dfe99e90d39c81b98")
+    .then(response => response.json())
+    .then(data => {
+        temp = Number(data.list[8].main.feels_like - 273.15).toFixed(1)
+        
+
+        //check what the actual spelling is
+        for(i = 0; i<5; i++) {
+            cond = String(data.list[8*i].weather[0].main)
+           if (cond == "Rain"){
+            document.getElementById("umbrella").innerHTML = "Don't forget to take an umbrella!";
+            break;
+           }
+        }
+
+        if (temp < -3.9) {
+            document.getElementById("head").innerHTML= "Head: Beanie";
+            document.getElementById("img_head").src = "./clothes/beanie.png"
+
+            document.getElementById("pant").innerHTML = "Pants: Woolen plants with thermals";
+            document.getElementById("img_pants").src = "./clothes/woolenpants.png"
+
+            document.getElementById("torso").innerHTML = "Torso: Winter Jacket";
+
+            document.getElementById("shoes").innerHTML = "Shoes: Boots";
+
+        } else if (temp < 6.67 ){
+            document.getElementById("head").innerHTML= "Head: Beanie";
+            document.getElementById("pant").innerHTML = "Pants: Woolen plants with thermals";
+            document.getElementById("torso").innerHTML = "Torso: Light to medium coat";
+            document.getElementById("shoes").innerHTML = "Shoes: Boots";
+
+        } else if (temp < 17.8) {
+            document.getElementById("head").innerHTML= "Head: No need!";
+            document.getElementById("pant").innerHTML = "Pants: Jeans";
+            document.getElementById("torso").innerHTML = "Torso: Fleece";
+            document.getElementById("shoes").innerHTML = "Shoes: Boots";
+
+        }else if (temp <26.1) {
+            document.getElementById("head").innerHTML= "Head: Whatever you want!";
+            document.getElementById("pant").innerHTML = "Pants: Chinos";
+            document.getElementById("torso").innerHTML = "Torso: Short sleeves";
+            document.getElementById("shoes").innerHTML = "Shoes: Tennis shoes";
+
+        } else  {
+            document.getElementById("head").innerHTML= "Head: Straw hat";
+            document.getElementById("pant").innerHTML = "Pants: Shorts";
+            document.getElementById("torso").innerHTML = "Torso: Short sleeves";
+            document.getElementById("shoes").innerHTML = "Shoes: Sandals";
+
+        }
+    })
+
+    
 }
